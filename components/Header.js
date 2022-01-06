fc5ad695ad842f1c18792ef211a31631c69d7b95
phil84mac <phil@thinkingbig.net>
2022-01-06 16:55:46 -0400

    get it started

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,99 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+import React, { useState, useEffect, MouseEvent, Component} from 'react';
+import { render } from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import Row from 'react-bootstrap/Row';
+import Stack from 'react-bootstrap/Stack';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import Link from 'next/link';
+import Offcanvas from 'react-bootstrap/Offcanvas'
+
+
+export default function Header() {
+
+  const [showOffCanvas, setShowOffCanvas] = useState(false);
+  const handleCloseOffCanvas = () => setShowOffCanvas(false);
+  const handleShowOffCanvas = () => setShowOffCanvas(true);
+
+  return (
+    <>
+
+      <Offcanvas show={showOffCanvas} onHide={handleCloseOffCanvas} placement='end'>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title className="mt-3" >
+          <p className="h6 text-dark-blue">GET STARTED - CONTACT US</p>
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body className="d-flex flex-column mb-3">
+        <h3 className=" mb-auto">
+        Interested in solving your problem with Thinking Big?
+          </h3>
+          
+          <Form className="">
+          <Form.Group className="mb-3" controlId="firstLastName">
+            <Form.Label className="p small text-grey">FIRST AND LAST NAME</Form.Label>
+            <Form.Control type="email" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="orgName">
+            <Form.Label className="p small text-grey">ORGANIZATION NAME</Form.Label>
+            <Form.Control type="email" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label className="p small text-grey">EMAIL ADDRESS</Form.Label>
+            <Form.Control type="email" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="lodaction">
+            <Form.Label className="p small text-grey">LOCATION</Form.Label>
+            <Form.Control type="email" placeholder="" />
+          </Form.Group>
+
+          <Form.Group className="mb-4" controlId="formGridState">
+            <Form.Label className="p small text-grey">EXPECTED NUMBER OF PLAYERS</Form.Label>
+            <Form.Select defaultValue="Please select...">
+              <option>Please select...</option>
+              <option>Less than 1000</option>
+              <option>1000-5000</option>
+              <option>5000-10,000</option>
+              <option>10,000-20,000</option>
+              <option>20,000 +</option>
+            </Form.Select>
+          </Form.Group>
+          <Button className="btn btn-primary w-full" type="submit">
+            SUBMIT
+          </Button>
+          </Form>
+        
+        </Offcanvas.Body>
+      </Offcanvas>
+    
+      <Navbar className="px-md-5 px-1" fixed="top">
+      <Navbar.Brand href="#home">
+        <img src="/TBlogo.svg" alt="Thinking Big Logo"/>
+      </Navbar.Brand>
+      <Nav className="ms-auto">
+        <Nav.Link href="#home">
+          <Button className="btn btn-primary arrow" onClick={handleShowOffCanvas}>
+            GET STARTED
+          </Button>
+        </Nav.Link>
+        <Nav.Link className="pe-0" href="#home">
+          <Button className="btn btn-primary menu">
+            
+          </Button>
+        </Nav.Link>
+       </Nav>
+      </Navbar>
+    
+</>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,88 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import React, { useState, useRef, useEffect } from 'react';
+import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import { useRouter } from 'next/router';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Placeholder from 'react-bootstrap/Placeholder';
+import Offcanvas from 'react-bootstrap/Offcanvas'
 
 export default function Home() {
   return (
-    <div className="container">
+
+    <>
+
+    <Container fluid className="px-0">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>THINKING BIG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Container fluid className="px-0 v-100 align-items-center d-flex">
+        <Row className="px-md-5">
+          <Col md={12}>
+          <h6 className="text-grey">THINKING BIG</h6>
+            <Row className="">
+              <Col md={6}>
+                <h1>
+                World-class software + consulting solutions
+                </h1>
+              </Col>
+              <Col md={6}>
+              <hr className="mt-2 mb-4"/>
+                <Row>
+                  <Col md={4}>
+                    <Link href="/">
+                    <a className="text-dark"><h4 className="d-arrow">Services</h4></a>
+                    </Link>
+                  </Col>
+                  <Col md={8}>
+                    <p className="text-grey mb-2">
+                    For 20 years, Thinking Big's interconnected services—software, web development and consulting—have helped clients of all sizes tackle their industry's toughest challenges. 
+                    </p>
+                  </Col>
+                </Row>
+              <hr className="mb-4" />
+                <Row>
+                  <Col md={4}>
+                  <a className="text-dark"><h4 className="d-arrow">Technology</h4></a>
+                  </Col>
+                  <Col md={8}>
+                    <p className="text-grey mb-2">
+                    Technology only works when it's the right choice for the right reasons. We're committed to innovation, matching the technology to the problem, and ultimately creating solutions that last.
+                    </p>
+                  </Col>
+                </Row>
+              <hr className="mb-4" />
+                <Row>
+                  <Col md={4}>
+                  <a className="text-dark"><h4 className="d-arrow">People</h4></a>
+                  </Col>
+                  <Col md={8}>
+                    <p className="text-grey mb-3">
+                    Our world-class group of developers, business consultants, designers and privacy specialists are ready to help you to transform your business.
+                    </p>
+                  </Col>
+                </Row>
+              <hr className="" />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        </Container>
       </main>
 
       <Footer />
-    </div>
+    </Container>
+    </>
   )
 }
