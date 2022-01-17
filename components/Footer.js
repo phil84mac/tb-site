42648191f341d29828ada88bd278a127374ebd27
phil84mac <phil@thinkingbig.net>
2022-01-17 10:44:36 -0400

    layout mostly done

--- components/Footer.js
+++ components/Footer.js
@@ -19,21 +19,21 @@ export default function Footer() {
     <>
       <footer className="no-brdr">
 
-      <Container fluid className="mb-4 mb-xl-5 pt-xl-3 pb-5 pb-xl-0">
+      <Container fluid className="mb-4 mb-xl-5 pt-xl-3 pb-sm-4 pb-xl-0">
         <Row className="mt-5 px-xl-5 px-lg-4 px-md-3 mb-xl-4">
-          <Col md={12} className="mt-4 pt-5">
-            <Row className="align-items-center d-flex mb-5">
-              <Col md={6} className="brdr-rght">
-                <h2 className="ms-0 my-sm-0 h1 text-dark">
+          <Col md={12} className="mt-5 mt-lg-4 pt-5">
+            <Row className="align-items-center d-flex mb-4 py-md-5 pb-lg-4 pt-lg-5 py-xl-3">
+              <Col md={6} className="brdr-rght-grey">
+                <h2 className="h1 text-dark">
                 Remake <br />  your future with us
                 </h2>
               </Col>
-               <Col md={6} xl={4} className="px-md-5 text-grey">
-                 <p className="mb-4">
+               <Col md={6} lg={4} xl={3} className="ps-md-5 text-grey">
+                 <p className="mb-3 lead">
                    For a competitive advantage you need intelligent solutions. At Thinking Big, we help organizations tackle and solve their own unique and complex challenges. Find out more.
                 </p>
                  <Button className="btn btn-primary arrow">
-                   Contact Us
+                   CONTACT US
                  </Button>
                </Col>
             </Row>
@@ -42,7 +42,7 @@ export default function Footer() {
               <Col xs={12} md={3}>
                 <p className="mono text-dark mb-4">&#169; THINKING BIG 2022</p>
               </Col>
-              <Col xs={12} md={3} className="mb-2">
+              <Col xs={12} md={3} className="mb-2 px-md-0">
               <p className="small text-grey mono ">OUR SITE</p>
                 <ul className="ps-4">
                   <li className="mb-2"><a href="#" className="text-dark">Option 1</a></li>
@@ -52,7 +52,7 @@ export default function Footer() {
                   <li className="mb-2"><a href="#" className="text-dark">Option 5</a></li>
                 </ul>
               </Col>
-              <Col xs={12} md={3} className="mb-2">
+              <Col xs={12} md={3} className="mb-2 px-md-0">
               <p className="small text-grey mono">SOCIAL MEDIA</p>
                 <ul className="ps-4">
                   <li className="mb-2"><a href="#" className="text-dark">Option 1</a></li>
@@ -62,7 +62,7 @@ export default function Footer() {
                   <li className="mb-2"><a href="#" className="text-dark">Option 5</a></li>
                 </ul>
               </Col>
-              <Col xs={12} md={3} className="mb-2">
+              <Col xs={12} md={3} className="mb-2 px-md-0">
               <p className="small text-grey mono">CONTACT INFO</p>
               <ul className="ps-4">
                   <li className="mb-2"><a href="#" className="text-dark">Option 1</a></li>
